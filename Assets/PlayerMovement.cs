50f44ac6d9b8e6ad729327f2b2ae863edcd64a30
Grainbox <tagemania@gmail.com>
2025-03-29 19:56:56 +0900

    Initial Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector2 moveInput;
+    public float speed = 5f;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move input: {moveInput}");
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, 0);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
