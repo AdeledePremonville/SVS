58ab9278c5be735a6019433702ba932456e2db62
Grainbox <tagemania@gmail.com>
2025-03-30 01:01:42 +0900

    [Feat]: Spawning players with custom controllers

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameSpawner : MonoBehaviour
+{
+    public GameObject characterPrefab;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        SpawnPlayer(
+            GameManager.Instance.player1Character,
+            GameManager.Instance.player1Device,
+            spawnPoints[0],
+            0
+        );
+
+        SpawnPlayer(
+            GameManager.Instance.player2Character,
+            GameManager.Instance.player2Device,
+            spawnPoints[1],
+            1
+        );
+    }
+
+    void SpawnPlayer(string characterName, InputDevice device, Transform spawnPoint, int playerIndex)
+    {
+
+        var player = PlayerInput.Instantiate(
+            characterPrefab,
+            playerIndex: playerIndex,
+            controlScheme: null,
+            pairWithDevice: device
+        );
+
+        player.transform.position = spawnPoint.position;
+    }
+
+    // GameObject GetPrefabByName(string name)
+    // {
+    //     foreach (var prefab in characterPrefabs)
+    //     {
+    //         if (prefab.name == name)
+    //             return prefab;
+    //     }
+    //
+    //     Debug.LogError($"Character prefab not found: {name}");
+    //     return null;
+    // }
+}
