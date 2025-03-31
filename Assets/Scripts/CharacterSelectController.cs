01355be1d0884d28981e9bb81f5062b1901477bf
Grainbox <tagemania@gmail.com>
2025-03-31 22:32:02 +0900

    [Feat]: Integrate input devices

--- Assets/Scripts/CharacterSelectController.cs
+++ Assets/Scripts/CharacterSelectController.cs
@@ -20,13 +20,13 @@ public class CharacterSelectController : MonoBehaviour
             labelRect.anchorMax = new Vector2(0, 0.5f);
             labelRect.anchoredPosition = new Vector2(100f, 0f);
             labelRect.pivot = new Vector2(0, 0.5f);
-            GameManager.Instance.player1Device = input.devices[0];
+            GameManager.Instance.Player1Device = input.devices[0];
         } else {
             labelRect.anchorMin = new Vector2(1, 0.5f);
             labelRect.anchorMax = new Vector2(1, 0.5f);
             labelRect.anchoredPosition = new Vector2(-100f, 0f);
             labelRect.pivot = new Vector2(1, 0.5f);
-            GameManager.Instance.player2Device = input.devices[0];
+            GameManager.Instance.Player2Device = input.devices[0];
         }
         
         OnReady();
