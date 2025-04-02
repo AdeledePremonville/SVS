ae7360ae3150f6a74260b7cc5ff26d49428daf8c
Grainbox <tagemania@gmail.com>
2025-04-02 10:41:23 +0900

    [Feat]: CharacterSelector

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Managers
+{
+    
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        public string player1Character;
+        public string player2Character;
+        public InputDevice Player1Device;
+        public InputDevice Player2Device;
+
+        public string gameMode; // "Solo", "Versus"
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+}
+
---
+++ Assets/Scripts/Menus/PrefabRenderer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Menus
+{
+
+    public class PrefabRenderer : MonoBehaviour
+    {
+        public GameObject[] prefabs;
+        private GameObject _instance;
+
+        void Start()
+        {
+            if (prefabs == null || prefabs.Length == 0)
+            {
+                Debug.LogError("[PrefabRenderer]: Prefab array is null");
+                return;
+            }
+
+            var index = 0;
+            foreach (var prefab in prefabs)
+            {
+                _instance = Instantiate(prefab);
+                _instance.layer = LayerMask.NameToLayer("RenderLayer");
+                
+                Debug.Log("[PrefabRenderer]: " + prefab.name + " is instantiated");
+                
+                // Stop considering instances like players
+                foreach (var script in _instance.GetComponents<MonoBehaviour>())
+                {
+                    Destroy(script);
+                }
+                
+                index++;
+            }
+        }
+    }
+
+}
