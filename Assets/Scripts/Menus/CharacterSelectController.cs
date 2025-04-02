06d8ecc85444b27b2093faaa12d660f4e37e9ef7
Grainbox <tagemania@gmail.com>
2025-04-02 12:18:17 +0900

    [Feat]: Spawn selected character

--- Assets/Scripts/Menus/CharacterSelectController.cs
+++ Assets/Scripts/Menus/CharacterSelectController.cs
@@ -6,10 +6,10 @@ namespace Menus
 {
     public class CharacterSelectController : MonoBehaviour
     {
-        public GameObject selectorPrefab;
-        
         private Controls _controls;
         private GameObject[] _characters;
+        private int _selectedCharIndex = 0;
+        private int _playerIndex = -1;
 
         private void Awake()
         {
@@ -17,11 +17,14 @@ namespace Menus
 
             _controls.Menu.MoveLeft.performed += _ => OnMoveLeft();
             _controls.Menu.MoveRight.performed += _ => OnMoveRight();
+            _controls.Menu.Ready.performed += _ => OnReady();
             
             var input = GetComponent<PlayerInput>();
             if (!input) return;
+            
+            _playerIndex = input.playerIndex;
 
-            if (input.playerIndex == 0)
+            if (_playerIndex == 0)
             {
                 Managers.GameManager.Instance.Player1Device = input.devices[0];
                 SpawnSelector();
@@ -35,15 +38,81 @@ namespace Menus
             {
                 _characters = GameObject.FindGameObjectsWithTag("Player");
             }
-
-            if (_characters.Length >= 1)
+            if (_characters.Length < 1)
             {
-                var position = new Vector3(_characters[0].transform.position.x, _characters[0].transform.position.y, _characters[0].transform.position.z);
-                Instantiate(selectorPrefab, position, Quaternion.identity);
-                Debug.Log(_characters[0].transform.position);
+                Debug.LogError("[CharacterSelectController]: No characters found");
+                return;
+            }
+            
+            UpdateSelector();
+        }
+
+        private static void DisableSelector(MeshRenderer selector)
+        {
+            if (selector != null)
+            {
+                selector.enabled = false;
+            }
+            else
+            {
+                Debug.LogWarning("[CharacterSelectController]: Unable to find selector quad or mesh controller !");
             }
         }
 
+        private static void EnableSelector(MeshRenderer selector)
+        {
+            if (selector != null)
+            {
+                selector.enabled = true;
+            }
+            else
+            {
+                Debug.LogWarning("[CharacterSelectController]: Unable to find selector quad or mesh controller !");
+            }
+        }
+
+        private void UpdateSelector(int increment = 0)
+        {
+            if (_characters is { Length: > 0 } && _characters[_selectedCharIndex] != null)
+            {
+                var selector = _characters[_selectedCharIndex].transform.Find("SelectorQuad");
+                if (selector != null)
+                {
+                    DisableSelector(selector.GetComponent<MeshRenderer>());
+                }
+                else
+                {
+                    Debug.LogWarning($"[CharacterSelectController]: SelectorQuad not found on character at index {_selectedCharIndex}");
+                }
+            }
+
+            if (_characters == null || _characters.Length == 0)
+                return;
+
+            if (_selectedCharIndex + increment >= _characters.Length)
+            {
+                _selectedCharIndex = 0;
+            }
+            else if (_selectedCharIndex + increment < 0)
+            {
+                _selectedCharIndex = _characters.Length - 1;
+            }
+            else
+            {
+                _selectedCharIndex += increment;
+            }
+
+            var nextSelector = _characters[_selectedCharIndex]?.transform.Find("SelectorQuad");
+            if (nextSelector != null)
+            {
+                EnableSelector(nextSelector.GetComponent<MeshRenderer>());
+            }
+            else
+            {
+                Debug.LogWarning($"[CharacterSelectController]: SelectorQuad not found on new character at index {_selectedCharIndex}");
+            }
+        }
+        
         private void OnEnable()
         {
             _controls.Menu.Enable();
@@ -54,19 +123,32 @@ namespace Menus
             _controls.Menu.Disable();
         }
 
-        private static void OnMoveLeft()
+        private void OnMoveLeft()
         {
-            Debug.Log("Move Left (A)");
+            UpdateSelector(-1);
         }
 
-        private static void OnMoveRight()
+        private void OnMoveRight()
         {
-            Debug.Log("Move Right (D)");
+            UpdateSelector(1);
         }
 
-        public void StartGame()
+        private void OnReady()
         {
+            if (_characters[_selectedCharIndex] == null)
+                Debug.LogError("_characters[_selectedCharIndex] is null");
+            
+            if (_playerIndex == 0)
+            {
+                Managers.GameManager.Instance.player1Character = _characters[_selectedCharIndex].name;
+            }
+            else
+            {
+                Managers.GameManager.Instance.player2Character = _characters[_selectedCharIndex].name;
+            }
+            
             SceneManager.LoadScene("GameScene");
         }
+        
     }
 }
