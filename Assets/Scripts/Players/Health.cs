b16ce33d97fa471a4f18a686a815a42d0ac5f922
Yann Demuyt <demisistired@demissexroom.home>
2025-04-27 14:58:22 +0900

    Feat(Hitboxes+Damage): Created Hitbox Scripts that can be added to objects with colliders. In PlayerMovement the hitboxes can be activated for the different movement, ie: Kick punch. GameObjects with IDamageable can take damage. Made a temp script Health.cs that take IDamageable as a base class.

---
+++ Assets/Scripts/Players/Health.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public Material normalMaterial; // e.g., blue
+    public Material redMaterial;    // flashing color
+
+    private Renderer characterRenderer;
+
+    void Start() {
+        currentHealth = maxHealth;
+        characterRenderer = GetComponentInChildren<Renderer>();
+
+        if (characterRenderer != null && normalMaterial != null)
+        {
+            characterRenderer.material = normalMaterial;
+        }
+    }
+
+    public void TakeDamage(int amount) {
+        currentHealth -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage. Health: {currentHealth}");
+
+        if (characterRenderer != null)
+        {
+            StartCoroutine(FlashRed());
+        }
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    private System.Collections.IEnumerator FlashRed()
+    {
+        characterRenderer.material = redMaterial;
+        yield return new WaitForSeconds(0.15f); // Flash duration
+        characterRenderer.material = normalMaterial;
+    }
+
+    private void Die() {
+        Debug.Log($"{gameObject.name} died!");
+        // Add death animation or disable here
+    }
+}
